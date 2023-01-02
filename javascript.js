bec9cc459f9b5373de50920c14daa59343788412
BaoWoW <baonguyen9392@gmail.com>
2023-01-01 17:21:06 -0800

    Create Chessboard With Knight Image Test

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
-console.log('hello')
+let board = document.querySelector(".board");
+
+(function createBoard() {
+  let color = 'white';
+  //create rows
+      for (let d = 0; d < 8; d++) {
+        (color == 'white') ? color = 'black': color = 'white';
+
+        for(let i = 0; i < 8; i++) {
+          let cell = document.createElement('cell')
+          cell.textContent = `${d}-${i}`;
+          (color == 'white') ? color = 'black': color = 'white';  
+          cell.className = `cell-${color}`;
+          if (i === 4) {
+            cell.style.backgroundImage = "url(./knight.png)";
+            cell.style.backgroundRepeat = "no-repeat";
+            cell.style.backgroundPosition = "center";
+          }
+          board.appendChild(cell)
+        }   
+      }
+return board;
+})()
