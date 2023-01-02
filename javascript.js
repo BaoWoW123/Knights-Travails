718c5b4493c40131817a55b4688d0ac57d3cc1fb
BaoWoW <baonguyen9392@gmail.com>
2023-01-02 13:04:40 -0800

    Add Knight Piece To Board
    Knight can move to any cell on board with click event.
    Buttons are non-functional.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 let board = document.querySelector(".board");
+let cells = document.querySelectorAll('cell');
+let placeKnightBtn = document.querySelector('.placeKnightBtn');
+let locationBtn = document.querySelector('.locationBtn');
+let startBtn = document.querySelector('.start');
+let knight = document.createElement('img');
+knight.src = './knight.png';
 
 (function createBoard() {
   let color = 'white';
@@ -8,16 +14,26 @@ let board = document.querySelector(".board");
 
         for(let i = 0; i < 8; i++) {
           let cell = document.createElement('cell')
-          cell.textContent = `${d}-${i}`;
+          cell.dataset.id = `${d},${i}`;
           (color == 'white') ? color = 'black': color = 'white';  
-          cell.className = `cell-${color}`;
-          if (i === 4) {
-            cell.style.backgroundImage = "url(./knight.png)";
-            cell.style.backgroundRepeat = "no-repeat";
-            cell.style.backgroundPosition = "center";
-          }
+          cell.className = `cell ${color}`;
+          cell.addEventListener('click', () => {
+            (cell.hasChild) ? 
+            cell.removeChild:
+            cell.appendChild(knight) ;
+          })
           board.appendChild(cell)
         }   
       }
 return board;
 })()
+
+//non-functional
+locationBtn.addEventListener('click', () => {
+  console.log('btn clicked')
+  cells.forEach(cell => {
+    console.log('i')
+    if (cell.hasChildNodes) return console.log('child');
+    else return console.log('no knight found')
+  })
+})
